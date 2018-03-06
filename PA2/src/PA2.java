4514c23651538ca2cc2ccced99491be488a29698
e0008233 <howard.zhang.1995@gmail.com>
2018-03-05 18:31:03 -0800

    - DB statement

--- PA2/src/PA2.java
+++ PA2/src/PA2.java
@@ -1,77 +1,111 @@
+/*
+* CSE 132A PA2.java
+* Author: Zhang Hao
+* Date:   3 Mar 2018
+* 
+*/
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
+import java.sql.ResultSet;
 
-public class PA2
-{
-  public static void main(String[] args) throws ClassNotFoundException
-  {
-    // load the sqlite-JDBC driver using the current class loader
-    Class.forName("org.sqlite.JDBC");
+public class PA2 {
+	public static void main(String[] args) {
+		Connection conn = null; // Database connection.
 
-    System.out.println("hello");
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:D:/UCSD/CSE132A/PA2/data/pa2.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-      
+		try {
+			// Load the JDBC class.
+			Class.forName("org.sqlite.JDBC");
 
-      ResultSet rs = statement.executeQuery("select * from flight");
-      while(rs.next())
-      {
-        // read the result set
-    	String airName = rs.getString("airline");
-    	String origin= rs.getString("origin");
-    	String destination= rs.getString("destination");
-//        System.out.println("airline = " + airName);
-//        System.out.println("origin = " + origin);
-//        System.out.println("destination = " + destination);
-    	
-      }
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory", 
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-  }
-}
-class Vertex {
-	public String id;
-	public boolean visited;
-	public Set<String> edges = new HashSet<String>();
-	public Vertex(String airlineName){
-		id=airlineName;
-		visited=false;
+			
+			conn = DriverManager.getConnection("jdbc:sqlite:pa2.db");
+			
+			Statement dbStatemrnt = conn.createStatement();
+
+			// Drop all the table that might have been existed before 
 		
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Same_Flights;");
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Previous;"); 
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Current;");
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Connected;");
+
+			dbStatemrnt.executeUpdate(
+					"CREATE TABLE Current(Airline char(32), Origin char(32), Destination char(32), Stops int);");
+			dbStatemrnt.executeUpdate(
+					"CREATE TABLE Same_Flights(Airline char(32), Origin char(32), Destination char(32));");
+			dbStatemrnt.executeUpdate(
+					"CREATE TABLE Previous(Airline char(32), Origin char(32), Destination char(32), Stops int);");
+
+			// Insert tables with appropriate information
+			dbStatemrnt.executeUpdate(
+					"INSERT INTO Current(Airline, Origin, Destination) SELECT Airline, Origin, Destination FROM Flight;");
+			dbStatemrnt.executeUpdate(
+					"INSERT INTO Same_Flights(Airline, Origin, Destination) SELECT Airline, Origin, Destination FROM Flight;");
+
+			
+			ResultSet resultset = dbStatemrnt.executeQuery("SELECT COUNT(*) AS Size FROM Same_Flights;");
+		
+			
+			int flightNum = resultset.getInt("Size");
+			int stopNum   = 0;
+			
+			while(flightNum > 0){
+				if(stopNum > 0){
+					dbStatemrnt.executeUpdate("DELETE FROM Previous;");
+					dbStatemrnt.executeUpdate("INSERT INTO Previous SELECT * FROM Current;");
+					dbStatemrnt.executeUpdate("INSERT INTO Current SELECT s.Airline, s.Origin, f.Destination,"+stopNum+
+							" FROM Same_Flights s, Flight f" +
+							" WHERE s.Destination = f.Origin AND" +
+							" s.Airline = f.Airline AND" +
+							" s.Origin <> f.Destination;");
+					dbStatemrnt.executeUpdate("DELETE FROM Same_Flights;");
+					dbStatemrnt.executeUpdate(
+							"INSERT INTO Same_Flights SELECT Airline, Origin, Destination" + 
+									" FROM Current" + 
+									" EXCEPT SELECT Airline, Origin, Destination" + 
+							" FROM Previous;");
+
+				}
+				else{
+					dbStatemrnt.executeUpdate("INSERT INTO Current SELECT *," + stopNum +" FROM Flight;");
+					dbStatemrnt.executeUpdate("DELETE FROM Current WHERE Current.Stops IS NULL;");
+				}
+
+				resultset = dbStatemrnt.executeQuery("SELECT COUNT(*) AS Size FROM Same_Flights;");
+				flightNum = resultset.getInt("Size");
+				stopNum++;
+			}
+
+			
+			// Create the table Connected
+			
+			dbStatemrnt.executeUpdate("CREATE TABLE Connected(Airline char(32), Origin char(32), Destination char(32), Stops int);");
+			dbStatemrnt.executeUpdate("INSERT INTO Connected" + 
+					" SELECT Airline, Origin, Destination, MIN(Stops)" + 
+					" FROM Current" +
+					" GROUP BY Airline, Origin, Destination" + 
+					" ORDER BY Airline;");
+
+			// Drop all the computational tables
+		
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Previous;");
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Current;");
+			dbStatemrnt.executeUpdate("DROP TABLE IF EXISTS Same_Flights;");
+
+			// Close the ResultSet and Statement objects.
+			resultset.close();
+			dbStatemrnt.close();
+		} catch (Exception e) {
+			throw new RuntimeException("There was a runtime problem!", e);
+		} finally {
+			try {
+				if (conn != null) conn.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(
+						"Cannot close the connection!", e);
+			}
+		}
 	}
 }
-class Graph {
-
-	public Hashtable<String,Vertex> vertices;
-	public Graph() {
-		vertices = new Hashtable<String,Vertex>(); 
-	}
-
-
-};
--- PA2/src/Sample.java
+++ PA2/src/Sample.java
@@ -1,32 +1,74 @@
+//// CSE 132A Winter 2018, PA 2
+////
+//// Name: Zhang Hao
+//// PID: A15369522
+//// Lab Id: cs132abe
+////
+//
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 //import java.sql.ResultSet;
 //import java.sql.SQLException;
 //import java.sql.Statement;
+//import java.util.*;
 //
-//public class Sample
+//public class PA2
 //{
 //  public static void main(String[] args) throws ClassNotFoundException
 //  {
 //    // load the sqlite-JDBC driver using the current class loader
 //    Class.forName("org.sqlite.JDBC");
-//
 //    System.out.println("hello");
 //    Connection connection = null;
+//    Set<String> airlines = new HashSet<String>();
+//    Hashtable<String,Graph> airlineMaps = new  Hashtable<String,Graph>();
+//    String test = "Aberdeen";
 //    try
 //    {
 //      // create a database connection
-//      connection = DriverManager.getConnection("jdbc:sqlite:D:/Coding/Workspace/PA2/data/pa2.db");
+//      connection = DriverManager.getConnection("jdbc:sqlite:D:/UCSD/CSE132A/PA2/data/pa2.db");
 //      Statement statement = connection.createStatement();
 //      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-//      
-//
+//     
+//    
 //      ResultSet rs = statement.executeQuery("select * from flight");
 //      while(rs.next())
 //      {
 //        // read the result set
-//        System.out.println("name = " + rs.getString("name"));
-//        System.out.println("id = " + rs.getInt("id"));
+//    	String airName = rs.getString("airline");
+//    	String origin= rs.getString("origin");
+//    	String destination= rs.getString("destination");
+//    	if (test.equals(origin)) {
+//    		 System.out.println("airline = " + origin.length());
+//    	}
+//    	if (!airlines.contains(airName)){ 			  			 
+//    		airlineMaps.put(airName, new Graph(airName));
+//    	}
+//    	Graph airlineMap = airlineMaps.get(airName);
+//    	if (!airlineMaps.get(airName).vertices.contains(origin)) {
+//    		airlineMaps.get(airName).vertices.put(origin, new Vertex(origin));	
+//    	}
+//    	if (!airlineMaps.get(airName).vertices.contains(destination)) {
+//    		airlineMaps.get(airName).vertices.put(destination, new Vertex(destination));	
+//    	}
+//    	
+//    	airlineMaps.get(airName).vertices.get(origin).edges.add(destination);
+//    	
+////        System.out.println("airline = " + airName);
+////        System.out.println("origin = " + origin);
+////        System.out.println("destination = " + destination);
+//    	Graph test2 = airlineMaps.get("Delta");
+//        Set<String> keys = test2.vertices.keySet();
+//        for(String key: keys){
+//            System.out.println("Value of "+key+" is: "+test2.vertices.get(key).id);
+//        }
+//    	
+//      }
+//      System.out.println("-----------------");
+//      Graph test2 = airlineMaps.get("Delta");
+//      Set<String> keys = test2.vertices.keySet();
+//      for(String key: keys){
+//          System.out.println("Value of "+key+" is: "+test2.vertices.get(key).id);
 //      }
 //    }
 //    catch(SQLException e)
@@ -48,5 +90,42 @@
 //        System.err.println(e);
 //      }
 //    }
+//
+////    Graph test2 = airlineMaps.get("Delta");
+////    Set<String> keys = test2.vertices.keySet();
+////    for(String key: keys){
+////        System.out.println("Value of "+key+" is: "+test2.vertices.get(key).id);
+////    }
+////    
+//    
+////    Vertex test3		= test2.vertices.get(test);
+////    System.out.println(test3);	
+////    Iterator<String> it	= test3.edges.iterator();
+////    while(it.hasNext()){
+////        System.out.println("city = " + it.next());
+////     }
+////  }
+//
 //  }
+//  static class Vertex {
+//		public static String id;
+//		public static boolean visited;
+//		public static Set<String> edges = new HashSet<String>();
+//		public Vertex(String airlineName){
+//			id=airlineName;
+//			visited=false;
+//			
+//		}
+//	}
+//  static class Graph {
+//
+//		public static String name;
+//		public Hashtable<String,Vertex> vertices;
+//		public Graph(String airline) {
+//			name = airline;
+//			vertices = new Hashtable<String,Vertex>(); 
+//		}
+//		
+//
+//	}
 //}
